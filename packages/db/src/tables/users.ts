da048af73837449c96b4904ee0d16507ab41b7ac
alongw <neko@awwa.cc>
2025-02-18 20:13:09 +0800

    feat: add user table

---
+++ packages/db/src/tables/users.ts
@@ -0,0 +1,24 @@
+import { DataTypes, Model } from 'sequelize'
+import sequelize from './../index'
+
+export interface UserAttributes {
+    user_id: string
+    user_name: string
+    user_email: string
+}
+
+export const User = sequelize.define<Model<UserAttributes>>('Users', {
+    user_id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false
+    },
+    user_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user_email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
